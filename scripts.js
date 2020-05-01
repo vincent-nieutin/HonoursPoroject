6c582d6f8e1fd47654844173d8167f7e66950abc
Vincent <vincent.nieutin@yahoo.fr>
2020-05-01 16:17:26 +0100

    added opacity change to selected items

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   document.getElementById("hide_selected").addEventListener("click", (e)=>{
     if(selected){
-      document.getElementById(selected).setAttribute('visible',false);
+      document.getElementById(selected).setAttribute('visible','false');
       document.getElementById(selected).className ="";
       hiddenParts.push(selected);
       selected = null;
@@ -14,7 +14,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   document.getElementById("unhide_all").addEventListener("click", (e)=>{
     hiddenParts.forEach((item, i) => {
-      document.getElementById(item).setAttribute('visible',true);
+      document.getElementById(item).setAttribute('material','visible:true');
+      document.getElementById(item).setAttribute("material","opacity","1");
       document.getElementById(item).className ="interactible";
     });
     hiddenParts = [];
